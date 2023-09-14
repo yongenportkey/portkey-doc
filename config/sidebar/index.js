047d11587c546f41ceed8db05f516230471a9ef6
ykx <yangkexin@portkey.finance>
2023-09-14 14:24:21 +0800

    feat: 🎸 dev Quickstart and Troubleshooting

--- config/sidebar/index.js
+++ config/sidebar/index.js
@@ -1,11 +1,18 @@
-const Overview = require('./overview.json')
-const quickstart = require('./quickstart.json')
-const SDKs = require('./SDKs.json')
-const APIReference = require('./APIReference.json')
-
-const Environmental = require('./Environmental.json')
+const Overview = require("./overview.json");
+const quickstart = require("./quickstart.json");
+const SDKs = require("./SDKs.json");
+const APIReference = require("./APIReference.json");
+const Environmental = require("./Environmental.json");
+const Troubleshooting = require("./Troubleshooting.json");
 
 module.exports = {
-    "label": "Docs Sidebar",
-    "items": [Overview, quickstart, SDKs, APIReference, Environmental]
-}
+  label: "Docs Sidebar",
+  items: [
+    Overview,
+    quickstart,
+    SDKs,
+    APIReference,
+    Environmental,
+    Troubleshooting,
+  ],
+};
